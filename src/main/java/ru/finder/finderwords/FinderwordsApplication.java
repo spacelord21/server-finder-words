6cd0124224d7d26977e4ff4c5ada56d09dce67db
spacelord21 <incrediblespaceee@gmail.com>
2023-05-14 20:28:48 +0200

    init commit

---
+++ src/main/java/ru/finder/finderwords/FinderwordsApplication.java
@@ -0,0 +1,13 @@
+package ru.finder.finderwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/finder/finderwords/FinderwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.finder.finderwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
